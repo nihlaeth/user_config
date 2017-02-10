1dc4eddbcd111f351c04623b5578b7ad0d8a558a
nihlaeth <info@nihlaeth.nl>
2017-02-10 15:55:04 +0100

    setuptools configuration

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Installation script for user_config."""
+from setuptools import setup, find_packages
+
+setup(
+    name='user_config',
+    version='1.0',
+    description='manage user configuration for Python packages',
+    author='nihlaeth',
+    author_email='info@nihlaeth.nl',
+    python_requires='>=3.5',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'user_config.file_types': [
+            'ini = user_config.ini:register_extension']},
+    )
