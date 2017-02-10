8dcf7dcf19117dfd391b3d7107c703a07dcaac07
nihlaeth <info@nihlaeth.nl>
2017-02-10 17:18:43 +0100

    finish Config.__init__

--- user_config/__init__.py
+++ user_config/__init__.py
@@ -1,4 +1,6 @@
 """User config management."""
+from pathlib import Path
+import argparse
 from pkg_resources import iter_entry_points
 
 class ConfigElement(object):
@@ -62,15 +64,15 @@ def config_meta(cls_name, cls_parents, cls_attributes):
     """
     reserved_names = [
         '_elements',
-        '_user_extension',
-        '_global_extension',
-        '_reader',
-        '_writer',
-        '_validator']
+        '_extension',
+        '_read',
+        '_write',
+        '_validate']
     new_attributes = {'_elements': {}}
     for attribute in cls_attributes:
         if isinstance(cls_attributes[attribute], ConfigElement):
-            new_attributes['_elements'] = cls_attributes[attribute]
+            new_attributes['_elements'][attribute] = cls_attributes[attribute]
+            new_attributes['_elements'][attribute].name = attribute
         elif attribute == 'file_type':
             file_types = {}
             for entry_point in iter_entry_points('user_config.file_type'):
@@ -81,11 +83,10 @@ def config_meta(cls_name, cls_parents, cls_attributes):
                     'no entry point found for file type {}'.format(
                         cls_attributes[attribute]))
             extension = file_types[cls_attributes[attribute]].load()()
-            new_attributes['_user_extension'] = extension['user_extension']
-            new_attributes['_global_extension'] = extension['global_extension']
-            new_attributes['_reader'] = extension['reader']
-            new_attributes['_writer'] = extension['writer']
-            new_attributes['_validator'] = extension['validator']
+            new_attributes['_extension'] = extension['extension']
+            new_attributes['_read'] = extension['read']
+            new_attributes['_write'] = extension['write']
+            new_attributes['_validate'] = extension['validate']
         elif attribute in reserved_names:
             raise AttributeError(
                 '{} is a reserved attribute for Config classes'.format(
@@ -112,11 +113,6 @@ class Config(metaclass=config_meta):
     Base class for application configuration.
 
     TODO
-
-    Parameters
-    ----------
-    command_line_arguments: TODO
-        TODO
 
     Raises
     ------
@@ -146,15 +142,51 @@ class Config(metaclass=config_meta):
     file_type = "ini"
     application = None
 
-    def __init__(self, command_line_arguments):
+    def __init__(self):
         if self.application is None:
             raise AttributeError(
                 'application not set, please provide an application name')
-        # TODO: validate _elements
-        # TODO: populate _data with defaults
-        # TODO: get configuration paths
-        # TODO: read global config
-        # TODO: read user config
-        # TODO: construct a commandline parser
-        # TODO: put command line argument data into _data
-        # TODO: validate _data
+        self._data = {}
+        # validate _elements
+        self._validate(self._elements)
+        # populate _data
+        for element in self._elements:
+            if self._elements[element].has_default:
+                self._data[element] = self._elements[element].get_default()
+            else:
+                self._data[element] = None
+        # read global config
+        global_path = Path('/etc').joinpath("{}{}".format(
+            self.application, self._extension))
+        if not global_path.is_file():
+            alternate_global_path = global_path
+            global_path = Path('/etc').joinpath(self.application).joinpath(
+                "config{}".format(self._extension))
+        if global_path.is_file():
+            self._read(global_path, self._elements, self._data)
+        # read user config
+        user_path = Path().home().joinpath(".{}{}".format(
+            self.application, self._extension))
+        if not user_path.is_file():
+            alternate_user_path = user_path
+            user_path = Path().home().joinpath(".config").joinpath(
+                self.application).joinpath("config{}".format(self._extension))
+        if user_path.is_file():
+            self._read(user_path, self._elements, self._data)
+        # construct a commandline parser
+        parser = argparse.ArgumentParser(
+            prog=self._application,
+            description="{}\n\n{}\n{}\n{}".format(
+                self.__doc__,
+                "Command line arguments overwrite configuration found in:",
+                user_path if alternate_user_path is None else alternate_user_path,
+                global_path if alternate_global_path is None else alternate_global_path))
+        for element in self._elements:
+            self._elements[element].construct_parser(parser)
+        # put command line argument data into _data
+        command_line_arguments = parser.parse_args()
+        for element in self._elements:
+            self._elements[element].extract_data_from_parser(
+                command_line_arguments, self._data)
+            # validate _data
+            self._elements[element].validate_data(self._data)
