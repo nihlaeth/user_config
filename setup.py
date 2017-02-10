205fb02a9494d4e3ae333420449141e508617454
nihlaeth <info@nihlaeth.nl>
2017-02-10 19:41:10 +0100

    fix appdirs version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author_email='info@nihlaeth.nl',
     python_requires='>=3.5',
     packages=find_packages(),
-    install_requires=['appdirs>=1.4.1'],
+    install_requires=['appdirs>=1.4'],
     entry_points={
         'user_config.file_types': [
             'ini = user_config.ini:register_extension']},
