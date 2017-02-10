ce7558f88893cf4f0b05cba7eebf955a57204fb0
nihlaeth <info@nihlaeth.nl>
2017-02-10 16:19:40 +0100

    simple example

---
+++ examples/simple_example.py
@@ -0,0 +1,26 @@
+"""Usage example for user_config."""
+from user_config import Config, Section, StringOption, IntOption
+
+class MyConfig(Config):
+
+    """Let the user provide some simple information about themselves."""
+
+    application = "my_application"
+    general = Section(
+        name=StringOption(
+            doc="your name",
+            default="unknown person"),
+        age=IntOption(
+            doc="your age",
+            required=True))
+    address = Section(
+        street=StringOption(
+            doc="street including house number",
+            required=True),
+        city=StringOption(required=True),
+        required=False,
+        doc="shipping address")
+
+if __name__ == "__main__":
+    CONFIG = MyConfig()
+    print("hello there, {}!".format(CONFIG.general.name))
