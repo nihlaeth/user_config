807c1c80296923699f877716c8a4cc0c4f419874
nihlaeth <info@nihlaeth.nl>
2017-02-12 15:54:55 +0100

    try to resolve failed build on readthedocs

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     license='GPLv3',
     python_requires='>=2.7',
     packages=find_packages(),
-    setup_requires=['setuptools', 'pip>=9.0.1', 'pytest-runner'],
+    setup_requires=['setuptools', 'pip>=8.1.2', 'pytest-runner'],
     tests_require=['pytest'],
     install_requires=['appdirs>=1.4', 'pathlib;python_version<"3.3"'],
     extras_require={
