61029a1f4df03cae5afcf9b4090bf5408606dd95
nihlaeth <info@nihlaeth.nl>
2017-02-25 20:41:09 +0100

    use twine environment variables

--- scripts/pypi_deploy.sh
+++ scripts/pypi_deploy.sh
@@ -2,20 +2,21 @@
 set -e
 set +x # double-check that x is unset
 python setup.py sdist bdist_wheel --universal
-cat > .pypirc << EOF
-[distutils]
-index-servers=
-    pypi
-    testpypi
-
-[server-login]
-username = nihlaeth
-password = ${PYPI_PASSWORD}
-
-[testpypi]
-repository = https://testpypi.python.org/pypi
-
-[pypi]
-repository = https://upload.pypi.org/legacy/
-EOF
-twine upload --config-file ./.pypirc -r ${PYPI_REPOSITORY} dist/*
+touch ~/.pypirc
+#cat > .pypirc << EOF
+#[distutils]
+#index-servers=
+#    pypi
+#    testpypi
+#
+#[server-login]
+#username = nihlaeth
+#password = ${PYPI_PASSWORD}
+#
+#[testpypi]
+#repository = https://testpypi.python.org/pypi
+#
+#[pypi]
+#repository = https://upload.pypi.org/legacy/
+#EOF
+twine upload dist/*
