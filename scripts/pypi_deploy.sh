c6047d4c030bb66250e4f9abb5dead2cb469041b
nihlaeth <info@nihlaeth.nl>
2017-02-26 15:01:37 +0100

    only run deploy script on master branch

--- scripts/pypi_deploy.sh
+++ scripts/pypi_deploy.sh
@@ -18,9 +18,8 @@ repository = https://upload.pypi.org/legacy/
 username = ${TWINE_USERNAME}
 password = ${TWINE_PASSWORD}
 _EOF_
-if [ "${TRAVIS_PYTHON_VERSION}" = "3.6" ]; then
-    $(which twine) upload -r testpypi --skip-existing dist/*
-    if [ "${TRAVIS_TAG}" != "" ]; then
-        $(which twine) upload -r pypi --skip-existing dist/*
-    fi
+$(which twine) upload -r testpypi --skip-existing dist/*
+if [ "${TRAVIS_TAG}" != "" ]; then
+    $(which twine) upload -r pypi --skip-existing dist/*
 fi
+set +v
